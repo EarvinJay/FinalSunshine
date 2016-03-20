74875b496c19e49d28e553bc179020da50fd392c
EarvinJay <earvinjaybermudo21@gmail.com>
2016-03-20 14:09:28 +0800

    TestDb class created and impleented

--- app/src/main/java/com/example/android/sunshinefinal/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshinefinal/app/SettingsActivity.java
@@ -12,14 +12,7 @@ import android.preference.PreferenceManager;
 import com.example.android.sunshinefinal.app.R;
 
 
-/**
- * A {@link PreferenceActivity} that presents a set of application settings.
- * <p/>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
- */
+
 public class SettingsActivity extends PreferenceActivity
         implements Preference.OnPreferenceChangeListener {
 
