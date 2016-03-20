8dffab0d559075354d6df05ca87ab769295264a5
EarvinJay <earvinjaybermudo21@gmail.com>
2016-03-20 14:22:49 +0800

    FullTestSuite created

---
+++ app/src/androidTest/java/com/example/android/sunshinefinal/app/FullTestSuite.java
@@ -0,0 +1,22 @@
+package com.example.android.sunshinefinal.app;
+
+/**
+ * Created by srthg on 3/20/2016.
+ */
+
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
