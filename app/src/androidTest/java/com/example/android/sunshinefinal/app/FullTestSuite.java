451f315fdda6e34aeff0a845fbbab9efcda160a3
EarvinJay <earvinjaybermudo21@gmail.com>
2016-03-24 20:59:06 +0800

    Test Provider created and Test db edited

--- app/src/androidTest/java/com/example/android/sunshinefinal/app/FullTestSuite.java
+++ app/src/androidTest/java/com/example/android/sunshinefinal/app/FullTestSuite.java
@@ -1,12 +1,14 @@
-package com.example.android.sunshinefinal.app.data;
+package com.example.android.sunshinefinal.app;
 
 /**
- * Created by srthg on 3/21/2016.
+ * Created by srthg on 3/24/2016.
  */
+
 import android.test.suitebuilder.TestSuiteBuilder;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+
 
 public class FullTestSuite extends TestSuite {
     public static Test suite() {
--- app/src/main/java/com/example/android/sunshinefinal/app/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshinefinal/app/data/WeatherContract.java
@@ -31,7 +31,6 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
         // Column with the foreign key into the location table.
-        public static final String COLUMN_LOC_KEY = "location_id";
         public static final String COLUMN_LOCATION_SETTING = "location_settings";
         public static final String COLUMN_CITY_NAME = "city_name";
         public static final String COLUMN_COORD_LAT = "coord_lat";
